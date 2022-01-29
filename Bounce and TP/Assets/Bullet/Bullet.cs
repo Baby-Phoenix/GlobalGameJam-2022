43d9335cc7e963737b901b0e5945c4ae991caba6
HailToKiyohime <jin.chan@ontariotechu.net>
2022-01-29 17:25:59 -0500

    FPS frame

---
+++ Bounce and TP/Assets/Bullet/Bullet.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public Transform barrel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
